f745d86f7a9603cc3a81631c3a724d316ffb120d
liuz-zzz <liuz_z@163.com>
2025-05-04 20:53:03 +0800

    Update main.cpp
    测试串口

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,10 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+   Serial.printf("Hello World!\n");
 }
